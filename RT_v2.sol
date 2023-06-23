c6ad0819056e273bb3da2c7cd56a402e02db82ec
jimzord12 <w.tool@yahoo.gr>
2023-06-23 12:50:14 +0300

    Added OpenZeppelin libs and fixed import path to RT_v2

--- RT_v2.sol
+++ RT_v2.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.19 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract RewardingTool {
     using SafeERC20 for IERC20;
